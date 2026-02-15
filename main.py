83d7507cdcc604c95031ad8c978046bbf4b1cd29
Eduard Azimov <eduard.azimoff@yandex.ru>
2026-02-15 17:43:00 +0300

    Refactoring

--- main.py
+++ main.py
@@ -2,258 +2,236 @@ import psycopg2
 
 from psycopg2.extensions import connection
 
-USER_NAME = ""
-USER_PASSWORD = ""
+USER_NAME = "postgres"
+USER_PASSWORD = "secret123"
 
 def create_db(conn: connection) -> bool:
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+        try:
+            cur.execute(""" CREATE TABLE IF NOT EXISTS client (
+                                client_id SERIAL PRIMARY KEY,
+                                name VARCHAR(80) NOT NULL,
+                                surname VARCHAR(80) NOT NULL,
+                                email VARCHAR(320) NOT NULL UNIQUE,
+                                CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
+                                );""")
 
-    try:
-        cur.execute(""" CREATE TABLE IF NOT EXISTS client (
-                            client_id SERIAL PRIMARY KEY,
-                            first_name VARCHAR(80) NOT NULL,
-                            second_name VARCHAR(80) NOT NULL,
-                            email VARCHAR(320) NOT NULL UNIQUE);""")
+            cur.execute(""" CREATE TABLE IF NOT EXISTS phone (
+                                number_id SERIAL PRIMARY KEY,
+                                number VARCHAR(15) NOT NULL UNIQUE,
+                                client_id INTEGER NOT NULL REFERENCES client(client_id));""")
 
-        cur.execute(""" CREATE TABLE IF NOT EXISTS phone (
-                            number_id SERIAL PRIMARY KEY,
-                            client_id INTEGER NOT NULL REFERENCES client(client_id),
-                            number VARCHAR(15) UNIQUE);""")
+            conn.commit()
 
-        conn.commit()
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
 
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return True
+        return True
 
 def drop_db(conn: connection) -> bool:
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+        try:
+            cur.execute("""DROP TABLE IF EXISTS client CASCADE;""")
 
-    try:
-        cur.execute("""DROP TABLE IF EXISTS client CASCADE;""")
+            cur.execute("""DROP TABLE IF EXISTS phone CASCADE;""")
 
-        cur.execute("""DROP TABLE IF EXISTS phone CASCADE;""")
+            conn.commit()
 
-        conn.commit()
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
 
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return True
+        return True
 
 def add_client(conn: connection, first_name: str, second_name: str, email: str, phones: list[str]) -> bool:
-    cur = conn.cursor()
-
-    try:
-        cur.execute(""" INSERT INTO client (first_name, second_name, email)
-                              VALUES
-                                (%s, %s, %s)
-                              RETURNING client_id;""", [first_name, second_name, email])
-
-        client_id = cur.fetchone()[0]
-
-        for phone in phones:
-            cur.execute(""" INSERT INTO phone (client_id, number)
+    with conn.cursor() as cur:
+        try:
+            cur.execute(""" INSERT INTO client (name, surname, email)
                                   VALUES
-                                    (%s, %s);""", [client_id, phone])
+                                    (%s, %s, %s)
+                                  RETURNING client_id;""", [first_name, second_name, email])
 
-        conn.commit()
+            client_id = cur.fetchone()[0]
 
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
+            for phone in phones:
+                cur.execute(""" INSERT INTO phone (number, client_id)
+                                      VALUES
+                                        (%s, %s);""", [phone, client_id])
 
-    return True
+            conn.commit()
+
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
+
+        return True
 
 def add_phone(conn: connection, client_id: int, phone: str):
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+        try:
+            cur.execute(""" INSERT INTO phone (number, client_id)
+                                  VALUES
+                                    (%s, %s);""", [phone, client_id])
 
-    try:
-        cur.execute(""" INSERT INTO phone (client_id, number)
-                              VALUES
-                                (%s, %s);""", [client_id, phone])
+            conn.commit()
 
-        conn.commit()
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
 
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return True
+        return True
 
 def get_client_data(conn: connection, client_id: int):
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+        try:
+            cur.execute(""" SELECT * FROM client
+                                  WHERE client_id = %s;""", [client_id])
 
-    try:
-        cur.execute(""" SELECT * FROM client
-                              WHERE client_id = %s;""", [client_id])
+            record = cur.fetchone()
+            if record is None:
+                print("The client was not found!")
 
-        record = cur.fetchone()
-        if record is None:
-            print("Client wasn't found!")
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
 
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return record
+        return record
 
 def get_client_phone_data(conn: connection, client_id: int):
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+        try:
+            cur.execute(""" SELECT * FROM phone
+                                  WHERE client_id = %s;""", [client_id])
 
-    try:
-        cur.execute(""" SELECT * FROM phone
-                              WHERE client_id = %s;""", [client_id])
+            records = cur.fetchall()
+            if records is None:
+                phone_list = []
+            else:
+                phone_list = [record[1] for record in records]
 
-        records = cur.fetchall()
-        if records is None:
-            phone_list = []
-        else:
-            phone_list = [record[-1] for record in records]
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
 
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return phone_list
+        return phone_list
 
 
 def change_client_data(conn: connection, client_id: int,
                        first_name: str = None, second_name: str = None,
                        email: str = None, phones: list[str] = None):
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+        try:
+            cur.execute(""" SELECT name, surname, email
+                                  FROM client
+                                  WHERE client_id = %s""", [client_id])
 
-    try:
-        cur.execute(""" SELECT first_name, second_name, email
-                              FROM client
-                              WHERE client_id = %s""", [client_id])
+            update_name, update_surname, update_email = cur.fetchone()
 
-        update_first_name, update_second_name, update_email = cur.fetchone()
+            if first_name is not None:
+                update_name = first_name
+            if second_name is not None:
+                update_surname = second_name
+            if email is not None:
+                update_email = email
 
-        if first_name is not None:
-            update_first_name = first_name
-        if second_name is not None:
-            update_second_name = second_name
-        if email is not None:
-            update_email = email
+            cur.execute(""" UPDATE client
+                                  SET name = %s, surname = %s, email = %s
+                                  WHERE client_id = %s;""", [update_name, update_surname, update_email, client_id])
 
-        cur.execute(""" UPDATE client
-                              SET first_name = %s, second_name = %s, email = %s
-                              WHERE client_id = %s;""", [update_first_name, update_second_name, update_email, client_id])
+            if phones is not None:
+                cur.execute(""" DELETE FROM phone
+                                      WHERE client_id = %s;""", [client_id])
 
-        if phones is not None:
+                for phone in phones:
+                    cur.execute(""" INSERT INTO phone (number, client_id)
+                                          VALUES
+                                            (%s, %s);""", [phone, client_id])
+
+            conn.commit()
+
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
+
+        return True
+
+def delete_phone(conn: connection, client_id: int, phone: str):
+    with conn.cursor() as cur:
+        try:
+            if get_client_data(conn, client_id) is None:
+                return False
+
+            cur.execute(""" DELETE FROM phone
+                                  WHERE client_id = %s AND number = %s;""", [client_id, phone])
+
+            conn.commit()
+
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
+
+        return True
+
+def delete_client(conn: connection, client_id: int):
+    with conn.cursor() as cur:
+        try:
+            if get_client_data(conn, client_id) is None:
+                return False
+
+            cur.execute(""" DELETE FROM client
+                                  WHERE client_id = %s;""", [client_id])
+
             cur.execute(""" DELETE FROM phone
                                   WHERE client_id = %s;""", [client_id])
 
-            for phone in phones:
-                cur.execute(""" INSERT INTO phone (client_id, number)
-                                      VALUES
-                                        (%s, %s);""", [client_id, phone])
+            conn.commit()
 
-        conn.commit()
-
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return True
-
-def delete_phone(conn: connection, client_id: int, phone: str):
-    cur = conn.cursor()
-
-    try:
-        if get_client_data(conn, client_id) is None:
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
             return False
 
-        cur.execute(""" DELETE FROM phone
-                              WHERE client_id = %s AND number = %s;""", [client_id, phone])
-
-        conn.commit()
-
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return True
-
-def delete_client(conn: connection, client_id: int):
-    cur = conn.cursor()
-
-    try:
-        if get_client_data(conn, client_id) is None:
-            return False
-
-        cur.execute(""" DELETE FROM client
-                              WHERE client_id = %s;""", [client_id])
-
-        cur.execute(""" DELETE FROM phone
-                              WHERE client_id = %s;""", [client_id])
-
-        conn.commit()
-
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    return True
+        return True
 
 def find_client(conn: connection, first_name: str = None, second_name: str = None,
-                email: str = None, phone: str = None):
-    cur = conn.cursor()
+                email: str = None, phone_number: str = None):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(""" SELECT c.client_id FROM client AS c
+                                  LEFT JOIN phone AS p
+                                  ON c.client_id = p.client_id
+                                  WHERE ((name = %(name)s) OR (%(name)s IS NULL))
+                                  AND ((surname = %(surname)s) OR (%(surname)s IS NULL))
+                                  AND ((email = %(email)s) OR (%(email)s IS NULL))
+                                  AND ((number = %(number)s) OR (%(number)s IS NULL));""",
+                        {"name": first_name, "surname": second_name, "email": email, "number": phone_number})
 
-    try:
-        condition_list = []
-        condition_value_list = []
-        if first_name is not None:
-            condition_list.append("first_name = %s")
-            condition_value_list.append(first_name)
-        if second_name is not None:
-            condition_list.append("second_name = %s")
-            condition_value_list.append(second_name)
-        if email is not None:
-            condition_list.append("email = %s")
-            condition_value_list.append(email)
-
-        if condition_value_list:
-            cur.execute(f""" SELECT client_id FROM client
-                                   WHERE {' AND '.join(condition_list)}; """, condition_value_list)
             found_record = cur.fetchone()
+
+        except psycopg2.Error as ex:
+            print(ex)
+            conn.rollback()
+            return False
+
+        if found_record:
+            return found_record[0]
         else:
-            found_record = None
-
-        if found_record is None and phone:
-            cur.execute(f"""SELECT client_id FROM phone
-                                  WHERE number = %s;""", [phone])
-
-            found_record = cur.fetchone()
-
-        conn.commit()
-
-    except psycopg2.Error as ex:
-        print(ex)
-        conn.rollback()
-        return False
-
-    if found_record:
-        return found_record[0]
-    else:
-        return None
+            return None
 
 if __name__ == "__main__":
     with psycopg2.connect(database="clients_db" ,user=USER_NAME, password=USER_PASSWORD) as conn:
         cur = conn.cursor()
+
+        drop_db(conn)
 
         # CREATING TABLES
         create_db(conn)
@@ -267,7 +245,7 @@ if __name__ == "__main__":
         client_id_pavel = find_client(conn, first_name = "Pavel")
         client_id_stephen = find_client(conn, second_name = "Hawking")
         client_id_elon = find_client(conn, email = "elon.mask@gmail.com")
-        client_id_elon_found_by_number = find_client(conn, phone = "5555551234")
+        client_id_elon_found_by_number = find_client(conn, phone_number="5555551234")
 
         assert client_id_pavel == 1
         assert client_id_stephen == 2
@@ -305,5 +283,3 @@ if __name__ == "__main__":
 
         assert new_client_data_stephen == tuple([client_id_stephen, 'Anonymous', 'Hawkinggg', 'stephen.hawkinggg@gmail.com'])
         assert new_client_phone_data_stephen == ["123456789", "987654321"]
-
-        drop_db(conn)
